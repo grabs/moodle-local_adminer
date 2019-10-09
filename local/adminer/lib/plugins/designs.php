9c43f3ac431402d143e5f1420e7c41703686c0eb
Andreas Grabs <moodle@grabs-edv.de>
2019-10-09 22:56:23 +0200

    grabs-edv subtree merge from MOODLE_38_STABLE into v_38_subtree
    git-subtree-dir: local/adminer
    git-subtree-mainline: 6f2e1ca2af53741052b2d620c079e7f89a42670c
    git-subtree-split: 6f2e1ca2af53741052b2d620c079e7f89a42670c

---
+++ local/adminer/lib/plugins/designs.php
@@ -0,0 +1,17 @@
+<?php
+
+/** Allow switching designs
+* @link https://www.adminer.org/plugins/#use
+* @author Jakub Vrana, https://www.vrana.cz/
+* @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+* @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+*/
+class AdminerMdlDesigns {
+	function css() {
+        global $OUTPUT, $PAGE;
+        $PAGE->set_context(\context_system::instance());
+        // print_r($PAGE->theme->css_urls($PAGE));exit;
+        return $PAGE->theme->css_urls($PAGE);
+	}
+
+}
